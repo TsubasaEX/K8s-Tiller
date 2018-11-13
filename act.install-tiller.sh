9f6009c33f744c2c0b5e0ad3e9333dc44d085323
TsubasaEX <34151841+TsubasaEX@users.noreply.github.com>
2018-11-13 16:51:39 +0800

    add files

---
+++ act.install-tiller.sh
@@ -0,0 +1,30 @@
+#/bin/bash
+set -o xtrace
+set -o errexit
+
+
+cat << EOF | kubectl create -f -
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: tiller
+  namespace: kube-system
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: tiller
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: cluster-admin
+subjects:
+  - kind: ServiceAccount
+    name: tiller
+    namespace: kube-system
+EOF
+
+sleep 1
+
+helm init --service-account=tiller
+
